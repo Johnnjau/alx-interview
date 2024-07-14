75f69004c8710db1d720420d0a4fdd803e4373c1
John <johnnjau202@gmail.com>
2024-07-14 13:59:09 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,61 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-console.log(`Requesting URL: ${url}`);
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data. Status code:', response.statusCode);
-    return;
-  }
-
-  let film;
-  try {
-    film = JSON.parse(body);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
-    return;
-  }
-
-  const characters = film.characters;
-  if (!characters) {
-    console.error('No characters found in the film data.');
-    return;
-  }
-
-  const printChars = (i) => {
-    if (i === characters.length) return;
-
-    request(characters[i], (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-      if (charResponse.statusCode !== 200) {
-        console.error('Failed to fetch character data. Status code:', charResponse.statusCode);
-        return;
-      }
-
-      let character;
-      try {
-        character = JSON.parse(charBody);
-      } catch (charParseError) {
-        console.error('Error parsing JSON:', charParseError);
-        return;
-      }
-
-      console.log(character.name);
-      printChars(i + 1);
-    });
-  };
-
-  printChars(0);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const characters = JSON.parse(body).characters;
+  printChars(characters, 0);
 });
+const printChars = (characters, i) => {
+  if (i === characters.length) return;
+  request(characters[i], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    printChars(characters, i + 1);
+  });
+};
